4c6d09d21f9c46ffc248afb7d2b4a14f57945e8a
William <Williamomarramirezmartinez@gmail.com>
2020-02-16 16:00:31 -0500

    cambios

--- src/server.js
+++ src/server.js
@@ -1,11 +1,16 @@
+//Install express server
 const express = require('express');
 const path = require('path');
 
 const app = express();
 
-app.use(express.static(__dirname+'/dist/ng-blog'));
-app.get('/',function(req,res){
-    res.sendFile(path.join(__dirname+'/dist/ng-blog/index.html'));
+// Serve only the static files form the dist directory
+app.use(express.static('./dist/{{williamrmz}}'));
+
+app.get('/*', function(req,res) {
+    
+res.sendFile(path.join(__dirname,'/dist/{{williamrmz}}/index.html'));
 });
 
-app.listen(process.env.PORT || 8080);
+// Start the app by listening on the default Heroku port
+app.listen(process.env.PORT || 4200);
