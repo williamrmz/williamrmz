49e53301a010412bfe9e59c26b1dd877f689f374
William <Williamomarramirezmartinez@gmail.com>
2020-02-16 16:38:56 -0500

    intento 4

--- server.js
+++ server.js
@@ -3,8 +3,8 @@ const path = require('path');
 
 const app = express();
 
-app.use(express.static(__dirname+'/dist'));
-app.get('/',function(req,res){
+app.use(express.static(__dirname+'/dist/'));
+app.get('/*',function(req,res){
     res.sendFile(path.join(__dirname+'/dist/index.html'));
 });
 
